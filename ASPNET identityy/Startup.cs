a9ae0c1b7d70e363e42397a039259f852559c8ac
hadyyoussef025 <hadyyoussef025@gmail.com>
2023-04-12 10:06:39 +0200

    Add project files.

---
+++ ASPNET identityy/Startup.cs
@@ -0,0 +1,59 @@
+﻿using ASPNET_identityy.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNET_identityy.Startup))]
+namespace ASPNET_identityy
+{
+    public partial class Startup
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRoles();
+            CreateUsers();
+        }
+
+        public void CreateUsers()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var user = new ApplicationUser();
+            user.Email = "mobarmijoun@gmail.com";
+            user.UserName = "Khalid";
+            var check= userManager.Create(user,"Kh@l!d123");
+            if (check.Succeeded)
+            {
+                userManager.AddToRole(user.Id, "Admins");
+            }
+        }
+
+
+        public void CreateRoles()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            IdentityRole role ;
+            if (!roleManager.RoleExists("Admins"))
+            {
+                role = new IdentityRole();
+                role.Name = "Admins";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Authors"))
+            {
+                role = new IdentityRole();
+                role.Name = "Authors";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Readers"))
+            {
+                role = new IdentityRole();
+                role.Name = "Readers";
+                roleManager.Create(role);
+            }
+
+        }
+    }
+}
